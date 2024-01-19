99100cecf715ef3824ec299c703aa586b863126b
Alessandra Bombardi <ale.brbr@gmail.com>
2024-01-19 18:41:12 -0300

    adicionado exercício 05

---
+++ exercicios_5/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int cod1 = sc.nextInt();
+		int quant1 = sc.nextInt();
+		double valor1 = sc.nextDouble();
+		double valorTotal1 = valor1 * quant1;
+		
+		int cod2 = sc.nextInt();
+		int quant2 = sc.nextInt();
+		double valor2 = sc.nextDouble();
+		double valorTotal2 = valor2 * quant2;
+		
+		double valorFinal = valorTotal1 + valorTotal2;
+		System.out.printf("Valor a pagar: R$ %.2f %n", valorFinal);
+		sc.close();
+	}
+
+}
+
+/*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+Exemplos:
+Entrada: 
+12 1 5.30
+16 2 5.10
+Saída:
+VALOR A PAGAR: R$ 15.50
+Entrada: 
+13 2 15.30
+161 4 5.20
+Saída:
+VALOR A PAGAR: R$ 51.40
+Entrada: Saída:
+1 1 15.10
+2 1 15.10
+Saída:
+VALOR A PAGAR: R$ 30.20 */
