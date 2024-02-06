8a92055f0844db2dad5aedb76a2eaab462f63711
Alessandra Bombardi <ale.brbr@gmail.com>
2024-02-06 10:10:56 -0300

    adicionado exercício condicional 06

---
+++ exercicios_estrutura_condicional/exercicios_condicional_6/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double valor = sc.nextDouble();
+		
+		if (valor >= 0 && valor <= 25) {
+			System.out.println("Intervalo [0,25]");
+		} 
+		else if (valor >= 26 && valor <= 50) {
+			System.out.println("Intervalo [25,50]");
+		} 
+		else if (valor >= 51 && valor <= 75) {
+			System.out.println("Intervalo [50,75]");
+		} 
+		else if (valor >= 76 && valor <= 100) {
+			System.out.println("Intervalo [75,100]");
+		} else {
+			System.out.println("Fora de Intervalo");
+		}
+		
+		sc.close();
+	}
+
+}
+
+/* Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+Exemplos:
+Entrada: 
+25.01 
+Saída: Intervalo (25,50]
+Entrada: 
+25.00 
+Saída: Intervalo [0,25]
+Entrada: 
+100.00 
+Saída: Intervalo (75,100]
+Entrada: 
+-25.02 
+Saída: Fora de intervalo */
