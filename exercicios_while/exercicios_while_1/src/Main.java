7053cabc82f36dbddbbabb613ed43e7ed3d5a09e
Alessandra Bombardi <ale.brbr@gmail.com>
2024-02-14 13:36:05 -0300

    adicionado exercicio while 1

---
+++ exercicios_while/exercicios_while_1/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Inválida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+
+}
